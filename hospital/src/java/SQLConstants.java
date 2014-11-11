535c59974b587d62f6aec669bac3f6632c5deebc
David Jeong <djeong747@gmail.com>
2014-11-10 21:31:53 -0500

    Create sql constants file.

---
+++ hospital/src/java/SQLConstants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Class to house the query names for all SQL queries.
+ * @author j7jeong
+ */
+public class SQLConstants {
+    
+    public static String test = "";
+    
+    
+}

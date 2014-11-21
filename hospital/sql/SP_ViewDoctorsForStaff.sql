c000ebf6566d7f2e8281f72e954ab9d37f23c2de
David Jeong <djeong747@gmail.com>
2014-11-21 18:11:18 -0500

    One SP.

---
+++ hospital/sql/SP_ViewDoctorsForStaff.sql
@@ -0,0 +1,13 @@
+USE ece356;
+
+DELIMITER //
+CREATE PROCEDURE ViewDoctorsForStaff(IN user_name_ varchar(32))
+BEGIN
+SELECT u.legal_name, d.user_name, d.cpso_number, d.department
+FROM doctor_schema
+INNER JOIN doctor_schema d ON u.user_name = d.user_name
+INNER JOIN doctor_staff_assignment_schema ds ON ds.cpso_number = d.cpso_number
+WHERE ds.staff = user_name_
+ORDER BY u.legal_name ASC;
+END //
+DELIMITER ;

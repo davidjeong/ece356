30967d42aa7beac0bc147ecf43909f42a83be1dc
David Jeong <djeong747@gmail.com>
2014-11-21 00:37:23 -0500

    Protection against empty inputs.

--- hospital/src/java/org/hospital/servlet/LoginServlet.java
+++ hospital/src/java/org/hospital/servlet/LoginServlet.java
@@ -46,58 +46,66 @@ public class LoginServlet extends HttpServlet {
                 String userName = request.getParameter("username");
                 String password = request.getParameter("password");
 
-                cs = SQLConstants.CONN.prepareCall(SQLConstants.SELECT_VERIFY_USER);
-                int i=0;
-                cs.setString(++i, userName);
-                cs.setString(++i, password);
-                rs = cs.executeQuery();
+                if (!userName.isEmpty() && !password.isEmpty()) {
+                
+                    cs = SQLConstants.CONN.prepareCall(SQLConstants.SELECT_VERIFY_USER);
+                    int i=0;
+                    cs.setString(++i, userName);
+                    cs.setString(++i, password);
+                    rs = cs.executeQuery();
 
-                if (rs != null) {
-                    if (rs.next()) {
-                        String userType = rs.getString("user_type");
-                        String legalName = rs.getString("legal_name");
-                        SQLConstants.USER = new User();
-                        SQLConstants.USER.setLegalName(legalName);
-                        SQLConstants.USER.setUserName(userName);
-                        SQLConstants.USER.setPassword(password);
-                        SQLConstants.USER.setUserType(userType);
+                    if (rs != null) {
+                        if (rs.next()) {
+                            String userType = rs.getString("user_type");
+                            String legalName = rs.getString("legal_name");
+                            SQLConstants.USER = new User();
+                            SQLConstants.USER.setLegalName(legalName);
+                            SQLConstants.USER.setUserName(userName);
+                            SQLConstants.USER.setPassword(password);
+                            SQLConstants.USER.setUserType(userType);
 
-                        HttpSession session = request.getSession(true);
-                        session.setAttribute("legalname", SQLConstants.USER.getLegalName());
-                        session.setAttribute("username", SQLConstants.USER.getUserName());
-                        session.setAttribute("usertype", SQLConstants.USER.getUserType());
+                            HttpSession session = request.getSession(true);
+                            session.setAttribute("legalname", SQLConstants.USER.getLegalName());
+                            session.setAttribute("username", SQLConstants.USER.getUserName());
+                            session.setAttribute("usertype", SQLConstants.USER.getUserType());
+                        }
                     }
-                }
 
-                if (SQLConstants.USER == null) {
-                   logger.warn("No user");
-                   request.setAttribute("message", "User name or password is incorrect.");
-                   request.getRequestDispatcher("index.jsp").forward(request, response);
+                    if (SQLConstants.USER == null) {
+                       logger.warn("No user");
+                       request.setAttribute("message", "User name or password is incorrect.");
+                       request.getRequestDispatcher("index.jsp").forward(request, response);
+                    }
+                    else {
+                        logger.info("User found with user name [" + SQLConstants.USER.getUserName() + "], password [" + SQLConstants.USER.getPassword() + "]");
+
+                        //Redirect user based on user type
+                        boolean typeFound = false;
+                        if (SQLConstants.USER.getUserType().equals(SQLConstants.Doctor)) {
+                            typeFound = true;
+                            getServletContext().getRequestDispatcher("/jsp/doctor_ui.jsp").forward(request, response);
+                        }
+                        else if (SQLConstants.USER.getUserType().equals(SQLConstants.Patient)) {
+                            typeFound = true;
+                        }
+                        else if (SQLConstants.USER.getUserType().equals(SQLConstants.Staff)) {
+                            typeFound = true;
+                        }
+                        else if (SQLConstants.USER.getUserType().equals(SQLConstants.Finance)) {
+                            typeFound = true;
+                        }
+                        else if (SQLConstants.USER.getUserType().equals(SQLConstants.Legal)) {
+                            typeFound = true;
+                        }
+                        if (!typeFound) {
+                            logger.error("Couldn't find user type");
+                        }
+                    }
                 }
                 else {
-                    logger.info("User found with user name [" + SQLConstants.USER.getUserName() + "], password [" + SQLConstants.USER.getPassword() + "]");
-
-                    //Redirect user based on user type
-                    boolean typeFound = false;
-                    if (SQLConstants.USER.getUserType().equals(SQLConstants.Doctor)) {
-                        typeFound = true;
-                        getServletContext().getRequestDispatcher("/jsp/doctor_ui.jsp").forward(request, response);
-                    }
-                    else if (SQLConstants.USER.getUserType().equals(SQLConstants.Patient)) {
-                        typeFound = true;
-                    }
-                    else if (SQLConstants.USER.getUserType().equals(SQLConstants.Staff)) {
-                        typeFound = true;
-                    }
-                    else if (SQLConstants.USER.getUserType().equals(SQLConstants.Finance)) {
-                        typeFound = true;
-                    }
-                    else if (SQLConstants.USER.getUserType().equals(SQLConstants.Legal)) {
-                        typeFound = true;
-                    }
-                    if (!typeFound) {
-                        logger.error("Couldn't find user type");
-                    }
+                    logger.warn("Empty fields");
+                    request.setAttribute("message", "Fields are empty.");
+                    request.getRequestDispatcher("index.jsp").forward(request, response);
                 }
             }
             

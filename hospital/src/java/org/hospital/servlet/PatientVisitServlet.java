38a2ad5a7b426ce11d60cae0b56736fb20fda066
ach2chan <ach2chan@gmail.com>
2014-11-22 01:18:35 -0500

    updated view finance result again

---
+++ hospital/src/java/org/hospital/servlet/PatientVisitServlet.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hospital.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hospital.entities.Patient;
+import org.hospital.other.MySQLConnection;
+import org.hospital.other.SQLConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(urlPatterns = {"/PatientVisitServlet"})
+public class PatientVisitServlet extends HttpServlet {
+
+    Logger logger = LoggerFactory.getLogger(PatientVisitServlet.class);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        CallableStatement cs = null;
+        ResultSet rs = null;
+        int PatientVisits = 0;
+        
+        if (SQLConstants.CONN == null) {
+            MySQLConnection.establish();
+        }
+        try {
+            String cpso = request.getParameter("cpso");
+            String datetime = request.getParameter("searchtime");
+            logger.info(datetime);
+            if (cpso != "" && datetime != "") {
+                cs = SQLConstants.CONN.prepareCall(SQLConstants.VIEW_PATIENT_VISIT);
+                cs.setString(1, cpso);
+                cs.setString(2, datetime);
+                rs = cs.executeQuery();
+                while (rs.next())
+                {
+                    PatientVisits = rs.getInt("count(distinct v.patient_id)");
+                }
+            }
+        }            
+        catch (SQLException e)
+        {
+            logger.error(e.toString());
+        }
+        finally {
+            if (cs != null) {
+                try {
+                    cs.close();
+                } catch (SQLException ex) {
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                }
+            }
+            if (PatientVisits > 0) 
+            {
+                request.getSession().setAttribute("PatientVisits", PatientVisits);
+            }
+        }
+        getServletContext().getRequestDispatcher("/jsp/view_finance_results.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}

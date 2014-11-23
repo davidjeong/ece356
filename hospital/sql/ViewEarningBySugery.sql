daca58a28da0f55274953e406f98adcadce5ebae
Leo Zhao <y248zhao@gmail.com>
2014-11-23 01:49:34 -0500

    modified viewearningbysurgery query to take start/end time

--- hospital/sql/ViewEarningBySugery.sql
+++ hospital/sql/ViewEarningBySugery.sql
@@ -1,7 +1,7 @@
 USE ece356;
 
 DELIMITER //
-CREATE PROCEDURE ViewEarningBySugery(IN surgery_name_ VARCHAR(64))
+CREATE PROCEDURE ViewEarningBySugery(IN surgery_name_ VARCHAR(64), IN start_time_ DATETIME, IN end_time_ DATETIME)
 BEGIN
 	SELECT sum(s.cost) as earning_from_sugery_only
 	FROM 
@@ -11,6 +11,6 @@ BEGIN
 	ON
 		s.surgery_name = v.surgery_name
 	WHERE 
-		s.surgery_name = surgery_name_;
+		s.surgery_name = surgery_name_ and v.start_time >= start_time_ and v.end_time <= end_time_;
 END //
 DELIMITER ;

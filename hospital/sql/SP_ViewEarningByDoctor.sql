dc62fadc1d4b879b17ff7ff737e490d1122a601d
ach2chan <ach2chan@gmail.com>
2014-11-23 16:28:14 -0500

    changed naming scheme for ViewEarningByDoctor to align with other similar procedures

---
+++ hospital/sql/SP_ViewEarningByDoctor.sql
@@ -0,0 +1,16 @@
+USE ece356;
+
+DELIMITER //
+CREATE PROCEDURE ViewEarningByDoctor(IN cpso_number_ VARCHAR (6), IN start_time_ DATETIME, IN end_time_ DATETIME)
+BEGIN
+	SELECT sum(s.cost + v.standard_cost) as earning
+	FROM 
+		visit_schema v 
+	INNER JOIN
+		surgery_schema s
+	ON
+		s.surgery_name = v.surgery_name
+	WHERE 
+		v.cpso_number = cpso_number_ and v.start_time>=start_time_ and v.end_time <=end_time_;
+END //
+DELIMITER ;

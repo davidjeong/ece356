70931e1efc83e5a1288338db498dbdc13a3e02a7
Lawrence Fu <fuwengfai@gmail.com>
2014-11-23 23:29:20 -0500

    Added 'None' in surgery field and Added total revenue in finance

---
+++ hospital/sql/insert_none_surgery.sql
@@ -0,0 +1,9 @@
+INSERT INTO surgery_schema (surgery_name, cost) VALUES
+('None', '0');
+
+INSERT INTO visit_schema (patient_id, cpso_number, start_time, end_time, surgery_name, prescription, comments, diagnosis) VALUES
+('4', '000000', '2014-11-29:23:00:00', '2014-11-21:23:30:00', 'None', 'test', 'test', 'CAncer1'),
+('4', '000000', '2014-12-29:23:00:00', '2014-12-21:23:30:00', 'None', 'test', 'test', 'CAncer1');
+
+Call CountRevenueFromAllSurgeries('2013-11-29:23:00:00', '2016-11-21:23:30:00')
+select count(*) from visit_schema

f4e70f0083961a69b57a0253075fccf9f7215ed7
David Jeong <djeong747@gmail.com>
2014-11-24 17:18:11 -0500

    dollar symbols.

--- hospital/src/java/org/hospital/servlet/RevenueGeneratedServlet.java
+++ hospital/src/java/org/hospital/servlet/RevenueGeneratedServlet.java
@@ -133,7 +133,7 @@ public class RevenueGeneratedServlet extends HttpServlet {
                     
                     totalOutput.append("<tbody>");
                     totalOutput.append("<tr>");
-                    totalOutput.append("<td>").append(revenueFromSurgeries + revenueFromVisits).append("</td>");
+                    totalOutput.append("<td>").append("$ ").append(revenueFromSurgeries + revenueFromVisits).append(".00").append("</td>");
                     totalOutput.append("</tr>");
                     totalOutput.append("</tbody>");
 
@@ -159,7 +159,7 @@ public class RevenueGeneratedServlet extends HttpServlet {
                 visitsOutput.append("<td>").append(request.getParameter("start_range")).append("</td>");
                 visitsOutput.append("<td>").append(request.getParameter("end_range")).append("</td>");
                 visitsOutput.append("<td>").append(numberOfVisits).append("</td>");
-                visitsOutput.append("<td>").append(revenueFromVisits).append("</td>");
+                visitsOutput.append("<td>").append("$ ").append(revenueFromVisits).append(".00").append("</td>");
                 visitsOutput.append("</tr>");
                 visitsOutput.append("</tbody>");
                 visitsOutput.append("</table>");
@@ -183,7 +183,7 @@ public class RevenueGeneratedServlet extends HttpServlet {
                 surgeryOutput.append("<td>").append(request.getParameter("end_range")).append("</td>");
                 surgeryOutput.append("<td>").append(surgery_name).append("</td>");
                 surgeryOutput.append("<td>").append(numberOfSurgeries).append("</td>");
-                surgeryOutput.append("<td>").append(revenueFromSurgeries).append("</td>");
+                surgeryOutput.append("<td>").append("$ ").append(revenueFromSurgeries).append(".00").append("</td>");
                 surgeryOutput.append("</tr>");
                 surgeryOutput.append("</tbody>");
                 surgeryOutput.append("</table>");
